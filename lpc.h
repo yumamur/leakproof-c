7696fd8b83d197bdc20fec7ece5df02a9fb22011
yumamur <pccartman@gmail.com>
2023-10-13 18:45:02 +0300

    attribute definition fix

--- lpc.h
+++ lpc.h
@@ -25,8 +25,13 @@
 #ifndef LPC_H
 # define LPC_H
 
-int		lpc_export(void *addr, ...)
-		__attribute__((aligned(16), sentinel(0, 0)));
+# ifdef __clang__
+int	lpc_export(void *addr, ...) __attribute__((sentinel(0, 0)));
+# else
+#  ifdef __GNUC__
+int	lpc_export(void *addr, ...) __attribute__((sentinel(0)));
+#  endif
+# endif
 
 int		lpc_flush(void);
 
